f9781b0e13aed909aab020fc07bf9dbc4a1f4721
Hunter Chandler <wohonajax@gmail.com>
2025-08-28 23:52:59 -0500

    Rearrange MAYBE-ADD-NODE's docstring for clarity

--- networking.lisp
+++ networking.lisp
@@ -98,9 +98,9 @@ NODE must be closer to us than the kth closest node in the routing table."
               (node-distance-from-us (kth-closest-node-to-us)))))))
 
 (defun maybe-add-node (node)
-  "Splits the bucket NODE fits into if it's a candidate for splitting.
-Initiates the procedure for potentially adding a node to a bucket. Does nothing
-if NODE is already in the bucket."
+  "Does nothing if NODE is already in the routing table. If it isn't, splits
+the bucket NODE fits into if it's a candidate for splitting. Initiates the
+procedure for potentially adding a node to a bucket."
   (let ((bucket (correct-bucket node)))
     (when (contains node bucket :test #'eq)
       (return-from maybe-add-node))
