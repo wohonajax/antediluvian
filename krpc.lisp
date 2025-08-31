33537aeb5e7d45028c632b8ef8784f8a7f19654f
Hunter Chandler <wohonajax@gmail.com>
2025-08-31 00:28:29 -0500

    Revert "Add a layer of abstraction over dictionaries"
    This reverts commit f5d87aaac338db66c239893c765a28dae98f64db.

--- krpc.lisp
+++ krpc.lisp
@@ -24,78 +24,69 @@ Maps to info_hash when applicable.")
   "Creates a transaction ID and returns it as a byte-vector."
   (random-data 2))
 
-(defun make-dictionary ()
-  "Makes a dictionary for queries, responses, and error messages."
-  (make-hash-table :test #'equal))
-
-(defun add-to-dictionary (dictionary key value)
-  "Adds a pair KEY and VALUE to DICTIONARY."
-  (setf (gethash key dictionary) value))
-
-(defun retrieve-dictionary-value (dictionary key)
-  "Retrieves the value associated with KEY from DICTIONARY."
-  (gethash key dictionary))
-
-(defun pack-nodes-response (target)
-  "Packs the contact information for the k closest nodes to TARGET into compact
-format."
-  (reduce #'concat-vec (find-closest-nodes target) :key #'compact-node-info))
-
 ;;; Queries
 
 (defun ping-node (socket transaction-id)
   "Sends a ping message."
-  (let ((query-dictionary (make-dictionary))
-        (query-arguments (make-dictionary)))
-    (add-to-dictionary query-arguments "id" *id*)
-    (add-to-dictionary query-dictionary "t" transaction-id)
-    (add-to-dictionary query-dictionary "y" "q")
-    (add-to-dictionary query-dictionary "q" "ping")
-    (add-to-dictionary query-dictionary "a" query-arguments)
-    (setf (gethash transaction-id *transactions*) t)
-    (send-bencoded-data socket query-dictionary)))
+  (let ((query-dict (make-hash-table :test #'equal))
+        (query-arguments (make-hash-table :test #'equal)))
+    (setf (gethash "id" query-arguments) *id*
+
+          (gethash "t" query-dict) transaction-id
+          (gethash "y" query-dict) "q"
+          (gethash "q" query-dict) "ping"
+          (gethash "a" query-dict) query-arguments
+
+          (gethash transaction-id *transactions*) t)
+    (send-bencoded-data socket query-dict)))
 
 (defun find-node (socket node-id transaction-id)
   "Asks for a node's contact information."
-  (let ((query-dictionary (make-dictionary))
-        (query-arguments (make-dictionary)))
-    (add-to-dictionary query-arguments "id" *id*)
-    (add-to-dictionary query-arguments "target" node-id)
-    (add-to-dictionary query-dictionary "t" transaction-id)
-    (add-to-dictionary query-dictionary "y" "q")
-    (add-to-dictionary query-dictionary "q" "find_node")
-    (add-to-dictionary query-dictionary "a" query-arguments)
-    (setf (gethash transaction-id *transactions*) node-id)
-    (send-bencoded-data socket query-dictionary)))
+  (let ((query-dict (make-hash-table :test #'equal))
+        (query-arguments (make-hash-table :test #'equal)))
+    (setf (gethash "id" query-arguments) *id*
+          (gethash "target" query-arguments) node-id
+
+          (gethash "t" query-dict) transaction-id
+          (gethash "y" query-dict) "q"
+          (gethash "q" query-dict) "find_node"
+          (gethash "a" query-dict) query-arguments
+
+          (gethash transaction-id *transactions*) t)
+    (send-bencoded-data socket query-dict)))
 
 (defun get-peers (socket info-hash transaction-id)
   "Asks for peers under INFO-HASH."
-  (let ((query-dictionary (make-dictionary))
-        (query-arguments (make-dictionary)))
-    (add-to-dictionary query-arguments "id" *id*)
-    (add-to-dictionary query-arguments "info_hash" info-hash)
-    (add-to-dictionary query-dictionary "t" transaction-id)
-    (add-to-dictionary query-dictionary "y" "q")
-    (add-to-dictionary query-dictionary "q" "get_peers")
-    (add-to-dictionary query-dictionary "a" query-arguments)
-    (setf (gethash transaction-id *transactions*) info-hash)
-    (send-bencoded-data socket query-dictionary)))
+  (let ((query-dict (make-hash-table :test #'equal))
+        (query-arguments (make-hash-table :test #'equal)))
+    (setf (gethash "id" query-arguments) *id*
+          (gethash "info_hash" query-arguments) info-hash
+
+          (gethash "t" query-dict) transaction-id
+          (gethash "y" query-dict) "q"
+          (gethash "q" query-dict) "get_peers"
+          (gethash "a" query-dict) query-arguments
+
+          (gethash transaction-id *transactions*) info-hash)
+    (send-bencoded-data socket query-dict)))
 
 (defun announce-peer (socket info-hash transaction-id token)
   "Announces peer status under INFO-HASH."
-  (let ((query-dictionary (make-dictionary))
-        (query-arguments (make-dictionary)))
-    (add-to-dictionary query-arguments "id" *id*)
-    (add-to-dictionary query-arguments "implied_port" (if *use-implied-port-p* 1 0))
-    (add-to-dictionary query-arguments "info_hash" info-hash)
-    (add-to-dictionary query-arguments "port" *default-port*)
-    (add-to-dictionary query-arguments "token" token)
-    (add-to-dictionary query-dictionary "t" transaction-id)
-    (add-to-dictionary query-dictionary "y" "q")
-    (add-to-dictionary query-dictionary "q" "announce_peer")
-    (add-to-dictionary query-dictionary "a" query-arguments)
-    (setf (gethash transaction-id *transactions*) info-hash)
-    (send-bencoded-data socket query-dictionary)))
+  (let ((query-dict (make-hash-table :test #'equal))
+        (query-arguments (make-hash-table :test #'equal)))
+    (setf (gethash "id" query-arguments) *id*
+          (gethash "implied_port" query-arguments) (if *use-implied-port-p* 1 0)
+          (gethash "info_hash" query-arguments) info-hash
+          (gethash "port" query-arguments) *default-port*
+          (gethash "token" query-arguments) token
+
+          (gethash "t" query-dict) transaction-id
+          (gethash "y" query-dict) "q"
+          (gethash "q" query-dict) "announce_peer"
+          (gethash "a" query-dict) query-arguments
+
+          (gethash transaction-id *transactions*) info-hash)
+    (send-bencoded-data socket query-dict)))
 
 (defun send-message (type ip port transaction-id &key id info-hash)
   "Sends NODE a TYPE message. TYPE should be a keyword like
@@ -122,85 +113,93 @@ format."
 
 (defun respond-to-ping (socket dict node)
   "Responds to a ping query."
-  (let ((response-dictionary (make-dictionary))
-        (response-arguments (make-dictionary)))
-    (add-to-dictionary response-arguments "id" *id*)
-    (add-to-dictionary response-dictionary "t" (retrieve-dictionary-value dict "t"))
-    (add-to-dictionary response-dictionary "y" "r")
-    (add-to-dictionary response-dictionary "r" response-arguments)
-    (send-bencoded-data socket response-dictionary)))
+  (let ((response-dict (make-hash-table :test #'equal))
+        (response-arguments (make-hash-table :test #'equal)))
+    (setf (gethash "id" response-arguments) *id*
+
+          (gethash "t" response-dict) (gethash "t" dict)
+          (gethash "y" response-dict) "r"
+          (gethash "r" response-dict) response-arguments)
+    (send-bencoded-data socket response-dict)))
 
 (defun respond-to-find-node (socket dict node)
   "Responds to a find_node query."
-  (let ((response-dictionary (make-dictionary))
-        (response-arguments (make-dictionary))
-        (dict-arguments (retrieve-dictionary-value dict "a")))
-    (add-to-dictionary response-arguments "id" *id*)
-    (add-to-dictionary response-arguments "nodes"
-                       (let* ((target (retrieve-dictionary-value dict-arguments
-                                                                 "target"))
-                              (node-if-found (find-node-in-table target)))
-                         (if node-if-found
-                             (compact-node-info node-if-found)
-                             (pack-nodes-response target))))
-    (add-to-dictionary response-dictionary "t" (retrieve-dictionary-value dict "t"))
-    (add-to-dictionary response-dictionary "y" "r")
-    (add-to-dictionary response-dictionary "r" response-arguments)
-    (send-bencoded-data socket response-dictionary)))
+  (let ((response-dict (make-hash-table :test #'equal))
+        (response-arguments (make-hash-table :test #'equal))
+        (dict-arguments (gethash "a" dict)))
+    (setf (gethash "id" response-arguments) *id*
+
+          (gethash "nodes" response-arguments)
+          (let* ((target (gethash "target" dict-arguments))
+                 (node-if-found (find-node-in-table target)))
+            (if node-if-found
+                (compact-node-info node-if-found)
+                (reduce #'concat-vec (find-closest-nodes target)
+                        :key #'compact-node-info)))
+
+          (gethash "t" response-dict) (gethash "t" dict)
+          (gethash "y" response-dict) "r"
+          (gethash "r" response-dict) response-arguments)
+    (send-bencoded-data socket response-dict)))
 
 (defun respond-to-get-peers (socket dict node)
   "Responds to a get_peers query."
-  (let* ((dict-arguments (retrieve-dictionary-value dict "a"))
-         (hash (retrieve-dictionary-value dict-arguments "info_hash"))
+  (let* ((dict-arguments (gethash "a" dict))
+         (hash (gethash "info_hash" dict-arguments))
          (peers (have-peers hash))
-         (response-dictionary (make-dictionary))
-         (response-arguments (make-dictionary)))
-    (add-to-dictionary response-arguments "id" *id*)
-    (add-to-dictionary response-arguments "token" (invent-token hash node))
-    (add-to-dictionary response-dictionary "t" (retrieve-dictionary-value dict "t"))
-    (add-to-dictionary response-dictionary "y" "r")
-    (add-to-dictionary response-dictionary "r" response-arguments)
+         (response-dict (make-hash-table :test #'equal))
+         (response-arguments (make-hash-table :test #'equal)))
+    (setf (gethash "id" response-arguments) *id*
+          (gethash "token" response-arguments) (invent-token hash node)
+
+          (gethash "t" response-dict) (gethash "t" dict)
+          (gethash "y" response-dict) "r"
+          (gethash "r" response-dict) response-arguments)
     (if peers
-        (add-to-dictionary response-arguments "values" (mapcar #'compact-peer-info peers))
-        (add-to-dictionary response-arguments "nodes" (pack-nodes-response)))
-    (send-bencoded-data socket response-dictionary)))
+        (setf (gethash "values" response-arguments)
+              (mapcar #'compact-peer-info peers))
+        (setf (gethash "nodes" response-arguments)
+              (reduce #'concat-vec (find-closest-nodes hash)
+                      :key #'compact-node-info)))
+    (send-bencoded-data socket response-dict)))
 
 (defun respond-to-announce-peer (socket dict node source-port)
   "Responds to an announce_peer query. If the received token isn't valid,
 sends a protocol error message."
-  (let* ((response-dictionary (make-dictionary))
-         (response-arguments (make-dictionary))
-         (dict-arguments (retrieve-dictionary-value dict "a"))
-         (id (retrieve-dictionary-value dict-arguments "id"))
-         (implied-port-p (retrieve-dictionary-value dict-arguments "implied_port"))
-         (info-hash (retrieve-dictionary-value dict-arguments "info_hash"))
+  (let* ((response-dict (make-hash-table :test #'equal))
+         (response-arguments (make-hash-table :test #'equal))
+         (dict-arguments (gethash "a" dict))
+         (id (gethash "id" dict-arguments))
+         (implied-port-p (gethash "implied_port" dict-arguments))
+         (info-hash (gethash "info_hash" dict-arguments))
          (port (if (and implied-port-p (= implied-port-p 1))
                    ;; if implied_port is 1, use the source port
                    source-port
                    ;; otherwise use the supplied port
-                   (retrieve-dictionary-value dict-arguments "port")))
-         (token (retrieve-dictionary-value dict-arguments "token")))
+                   (gethash "port" dict-arguments)))
+         (token (gethash "token" dict-arguments)))
     (setf (node-port node) port)
-    (cond ((consider-token token info-hash node)
-           ;; TODO: add to peer list
-           (add-to-dictionary response-arguments "id" *id*)
-           (add-to-dictionary response-dictionary "t" (retrieve-dictionary-value dict "t"))
-           (add-to-dictionary response-dictionary "y" "r")
-           (add-to-dictionary response-dictionary "t" response-arguments)
-           (send-bencoded-data socket response-dictionary))
-          (t (dht-error socket :protocol dict)))))
+    (if (consider-token token info-hash node)
+        (progn (add-to-bucket node)
+               (setf (gethash "id" response-arguments) *id*
+
+                     (gethash "t" response-dict) (gethash "t" dict)
+                     (gethash "y" response-dict) "r"
+                     (gethash "r" response-dict) response-arguments)
+               (send-bencoded-data socket response-dict))
+        (dht-error socket :protocol dict))))
 
 (defun dht-error (socket type dict)
   "Sends a DHT error message."
-  (let ((error-dictionary (make-dictionary)))
-    (add-to-dictionary error-dictionary "t" (retrieve-dictionary-value dict "t"))
-    (add-to-dictionary error-dictionary "y" "e")
-    (add-to-dictionary error-dictionary "e" (case type
-                                              (:generic (list 201 "Generic Error"))
-                                              (:server (list 202 "Server Error"))
-                                              (:protocol (list 203 "Protocol Error"))
-                                              (:method (list 204 "Method Unknown"))))
-    (send-bencoded-data socket error-dictionary)))
+  (let ((error-dict (make-hash-table :test #'equal)))
+    (setf (gethash "t" error-dict) (gethash "t" dict)
+          (gethash "y" error-dict) "e"
+          (gethash "e" error-dict) (case type
+                                     (:generic (list 201 "Generic Error"))
+                                     (:server (list 202 "Server Error"))
+                                     (:protocol (list 203 "Protocol Error"))
+                                     (:method (list 204 "Method Unknown"))))
+    (send-bencoded-data socket error-dict)))
 
 (defun send-response (type node dict &key error-type source-port)
   (let ((ip (node-ip node))
