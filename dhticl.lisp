fd73f02b478ed6df92963561c19c59575a8b956a
Hunter Chandler <wohonajax@gmail.com>
2025-09-01 20:57:22 -0500

    Edit bootstrap nodes. Move start time to a local LOOP variable

--- dhticl.lisp
+++ dhticl.lisp
@@ -45,22 +45,24 @@
   ;; bootstrap the DHT with known nodes
   ;; (taken from qbittorrent's bootstrap list)
   (bootstrap-node "router.bittorrent.com" 6881)
+  (bootstrap-node "dht.libtorrent.org" 25401)
+  ;; FIXME: these 4 nodes don't seem to respond to pings
   (bootstrap-node "router.utorrent.com" 6881)
   (bootstrap-node "dht.transmissionbt.com" 6881)
-  (bootstrap-node "dht.libtorrent.org" 25401)
   (bootstrap-node "dht.aelitis.com" 6881)
+  (bootstrap-node "bootstrap.jami.net" 4222)
   ;; TODO: wait for bootstrapping before initiating lookups
   (initiate-lookups)
-  (let ((start-time (get-universal-time)))
-    (loop (parse-message)
-          ;; TODO: routing table upkeep
-          (when (= 0 (mod (minutes-since start-time) 10))
-            (iterate-table (lambda (bucket)
-                             (purge-stale-nodes bucket)
-                             (handle-questionable-nodes bucket)
-                             (purge-bad-nodes bucket)
-                             (ping-old-nodes bucket)))
-            (refresh-tokens)))))
+  (loop with start-time = (get-universal-time)
+        (parse-message)
+        ;; TODO: routing table upkeep
+        (when (= 0 (mod (minutes-since start-time) 10))
+          (iterate-table (lambda (bucket)
+                           (purge-stale-nodes bucket)
+                           (handle-questionable-nodes bucket)
+                           (purge-bad-nodes bucket)
+                           (ping-old-nodes bucket)))
+          (refresh-tokens))))
 
 (defun dht (&rest hashes)
   "Initiates the distributed hash table."
