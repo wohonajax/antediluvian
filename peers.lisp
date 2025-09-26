ef3f710bd6e1731fa5715a8925de6d0d1401f360
Hunter Chandler <wohonajax@gmail.com>
2025-09-26 12:59:24 -0500

    Fix up documentation for *PEER-LIST*. Add function to get a peer's socket object

--- peers.lisp
+++ peers.lisp
@@ -2,7 +2,8 @@
 
 (defvar *peer-list* (make-hash-table :test #'equalp)
   "A hash table containing info_hashes as keys and hash tables mapping IP
-addresses to socket objects as values.")
+addresses to socket object futures as values. These futures will have NIL
+values if the socket connection failed.")
 
 (defvar *listening-peer-socket* nil
   "A TCP socket listening for connections from peers.")
@@ -13,3 +14,10 @@ addresses to socket objects as values.")
     (unless peer-list
       (remhash info-hash *peer-list*))
     peer-list))
+
+(defun get-peer-socket (ip info-hash)
+  "Returns the socket object associated with IP for a peer under INFO-HASH, or
+NIL if the connection failed. Returns NIL if there is no such peer or if the
+connection failed. Will block if the connection attempt is still in progress."
+  (when-let (peers (gethash info-hash *peer-list*))
+    (force (gethash ip peers))))
