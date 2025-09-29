812e97e18835847e2f412f892429207618077f0a
Hunter Chandler <wohonajax@gmail.com>
2025-09-28 19:53:08 -0500

    We don't use individual DHT objects

--- lookups.lisp
+++ lookups.lisp
@@ -79,8 +79,7 @@ results are the same as the previous best results."
         ((equalp (gethash target *best-lookup-results*)
                  (gethash target *previous-best-lookup-results*))
          (remhash target *previous-best-lookup-results*)
-         (mapc (rcurry #'maybe-add-node (gethash target *hash-dhts*))
-               (gethash target *best-lookup-results*))
+         (mapc #'maybe-add-node (gethash target *best-lookup-results*))
          (remhash target *best-lookup-results*))
         ;; if the previous results aren't the same as the
         ;; current results, recurse on the results we got
