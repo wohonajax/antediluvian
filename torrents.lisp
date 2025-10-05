c3831c40b6318c24d95f47ce092416e6c6f981df
Hunter Chandler <wohonajax@gmail.com>
2025-10-05 17:45:46 -0500

    Parse sources into torrent objects

--- torrents.lisp
+++ torrents.lisp
@@ -2,14 +2,19 @@
 
 (in-package #:antediluvian)
 
+(defclass torrent ()
+  ((info-hash :initarg :info-hash :accessor torrent-info-hash)
+   (name :initarg :name :accessor torrent-name)
+   (destination-path :initarg :destination :accessor torrent-destination)))
+
 (defun magnet-link-p (item)
   "Tests whether ITEM is a magnet link."
   (and (stringp item) (starts-with-p "magnet:?" item)))
 
-(defun extract-sha1-from-magnet-link (magnet-link)
-  "Extracts a BitTorrent info hash from MAGNET-LINK."
+(defun extract-sha1-from-parsed-magnet-link (hash-table)
+  "Extracts a BitTorrent info hash from HASH-TABLE, a parsed magnet link."
   (loop for (protocol hash)
-          in (magnet:get-all-exact-topics (magnet:parse magnet-link))
+          in (magnet:get-all-exact-topics hash-table)
         when (eq protocol :btih)
           return hash))
 
@@ -18,22 +23,36 @@
   (with-open-file (stream filespec :element-type '(unsigned-byte 8))
     (bencode:decode stream)))
 
-(defun get-info-hash-from-torrent-file (filespec)
-  "Gets the SHA1 info hash from a torrent file referred to by FILESPEC."
-  (let ((dict (make-hash-table :test #'equal))
-        (torrent-file-dict (bdecode-torrent-file filespec)))
+(defun get-info-hash-from-bdecoded-torrent-file (hash-table)
+  "Gets the SHA1 info hash from HASH-TABLE, a decoded torrent file."
+  (let ((dict (make-hash-table :test #'equal)))
     ;; we need a dictionary of only the "info" part of the torrent file
-    (setf (gethash "info" dict) (gethash "info" torrent-file-dict))
+    (setf (gethash "info" dict) (gethash "info" hash-table))
     (digest-sequence :sha1 (bencode:encode dict nil))))
 
+(defun make-download-pathname (filename)
+  "Creates a download destination pathname from FILENAME."
+  (uiop:truenamize (merge-pathnames filename *default-download-directory*)))
+
 (defun parse-source (source)
-  "Parses SOURCE into a SHA1 hash. SOURCE should be a magnet link, filespec to
-a torrent file, or a SHA1 hash."
-  (cond ((magnet-link-p source)
-         (extract-sha1-from-magnet-link source))
-        ((or (stringp source) (pathnamep source))
-         (get-info-hash-from-torrent-file (truenamize source)))
-        (t source)))
+  "Parses SOURCE into a torrent object. SOURCE should be a magnet link,
+a filespec to a torrent file, or a SHA1 hash."
+  (let* ((parsed-source (cond ((magnet-link-p source)
+                               (magnet:parse source))
+                              ((filespecp source)
+                               (bdecode-torrent-file (uiop:truenamize source)))
+                              (t source)))
+         (hash (cond ((magnet-link-p source)
+                      (extract-sha1-from-parsed-magnet-link parsed-source))
+                     ((filespecp source)
+                      (get-info-hash-from-bdecoded-torrent-file parsed-source))
+                     (t source)))
+         (name (cond ((magnet-link-p source)
+                      (first (gethash :dn parsed-source)))
+                     ((filespecp source)
+                      (gethash "name" (gethash "info" parsed-source))))))
+    (make-instance 'torrent :info-hash hash :name name
+                            :destination (make-download-pathname name))))
 
 (defun parse-sources (list-of-sources)
   "Converts every source in LIST-OF-SOURCES to a SHA1 hash."
@@ -42,4 +61,4 @@ a torrent file, or a SHA1 hash."
 (defun add-torrent (source)
   "Adds a torrent from SOURCE, which should be a magnet link, a filespec to a
 torrent file, or a SHA1 hash."
-  (add-hash (parse-source source)))
+  (add-hash (torrent-info-hash (parse-source source))))
