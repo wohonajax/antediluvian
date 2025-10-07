684057e9843e04f02fcb0b9a7eaf9688b0c7f5d3
Hunter Chandler <wohonajax@gmail.com>
2025-10-07 17:23:30 -0500

    READ-SEQUENCE doesn't error on EOF

--- files.lisp
+++ files.lisp
@@ -10,14 +10,14 @@ we don't have the piece, or if PIECE-INDEX is out of bounds."
          (pieces (gethash "pieces" info-dictionary))
          (piece-length (gethash "piece length" info-dictionary))
          (sha1-index (* piece-index 20))
-         (sha1-hash (subseq pieces sha1-index (+ sha1-index 20)))
+         ;; if the piece is out of bounds, return nil
+         (sha1-hash (handler-case (subseq pieces sha1-index (+ sha1-index 20))
+                      (error () (return-from have-piece-p))))
          (file-destination-path (torrent-destination torrent)))
     (with-open-file (file-stream file-destination-path :element-type '(unsigned-byte 8))
       (file-position file-stream (* piece-index piece-length))
       (let ((result (make-array piece-length :element-type '(unsigned-byte 8))))
-        ;; if we get an end-of-file error, we're out of bounds. return nil
-        (handler-case (read-sequence result file-stream)
-          (end-of-file () (return-from have-piece-p)))
+        (read-sequence result file-stream)
         (equalp sha1-hash (digest-sequence :sha1 result))))))
 
 (defun write-piece (piece piece-index torrent)
