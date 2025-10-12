b6a48b834e7775bf49ee16268fed9d53163e10f5
Hunter Chandler <wohonajax@gmail.com>
2025-10-12 12:54:23 -0500

    Add slots to peer class

--- peers.lisp
+++ peers.lisp
@@ -3,4 +3,7 @@
 (in-package #:antediluvian)
 
 (defclass peer ()
-  (socket :accessor socket))
+  (socket :accessor peer-socket)
+  (had-pieces :accessor had-pieces)
+  (requested-pieces :accessor requested-pieces)
+  (chokedp :accessor chokedp))
