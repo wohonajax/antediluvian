40f8576e65d563d7a1112c6b6ee980de856a894c
Hunter Chandler <wohonajax@gmail.com>
2025-10-13 15:40:49 -0500

    Add choked and interested slots to peer class. Add explanatory comments

--- peers.lisp
+++ peers.lisp
@@ -3,7 +3,19 @@
 (in-package #:antediluvian)
 
 (defclass peer ()
+  ;; stream socket object for the connection to this peer
+  ;; wrapped in a future (need to use (force (peer-socket peer)))
   (socket :initarg :socket :accessor peer-socket)
+  ;; list of pieces this peer already has
   (had-pieces :initarg :had-pieces :accessor had-pieces)
+  ;; list of piece indices this peer has requested
   (requested-pieces :initarg :requested-pieces :accessor requested-pieces)
-  (chokedp :initarg :chokedp :accessor chokedp))
+  ;; whether we have choked this peer
+  (chokedp :initform t :accessor chokedp)
+  ;; whether this peer has choked us
+  (choked-us-p :initform t :accessor choked-us-p)
+  ;; whether we're interested in something this peer has
+  (interestedp :initform nil :accessor interestedp)
+  ;; whether this peer is interested in something we have
+  ;; if true then requests will be incoming when we unchoke
+  (interested-in-us-p :initform nil :accessor interested-in-us-p))
