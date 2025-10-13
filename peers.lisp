87a4caabda5d381d149592aea184e3759a581135
Hunter Chandler <wohonajax@gmail.com>
2025-10-13 16:32:10 -0500

    Class slots need to be a list of slot definitions, not spliced into the DEFCLASS form

--- peers.lisp
+++ peers.lisp
@@ -5,20 +5,20 @@
 (defclass peer ()
   ;; stream socket object for the connection to this peer
   ;; wrapped in a future (need to use (force (peer-socket peer)))
-  (socket :initarg :socket :accessor peer-socket)
-  ;; list of pieces this peer already has
-  (had-pieces :initform '() :accessor had-pieces)
-  ;; list of piece indices this peer has requested
-  (requested-pieces :initform '() :accessor requested-pieces)
-  ;; whether we have choked this peer
-  (chokedp :initform t :accessor chokedp)
-  ;; whether this peer has choked us
-  (choked-us-p :initform t :accessor choked-us-p)
-  ;; whether we're interested in something this peer has
-  (interestedp :initform nil :accessor interestedp)
-  ;; whether this peer is interested in something we have
-  ;; if true then requests will be incoming when we unchoke
-  (interested-in-us-p :initform nil :accessor interested-in-us-p))
+  ((socket :initarg :socket :accessor peer-socket)
+   ;; list of pieces this peer already has
+   (had-pieces :initform '() :accessor had-pieces)
+   ;; list of piece indices this peer has requested
+   (requested-pieces :initform '() :accessor requested-pieces)
+   ;; whether we have choked this peer
+   (chokedp :initform t :accessor chokedp)
+   ;; whether this peer has choked us
+   (choked-us-p :initform t :accessor choked-us-p)
+   ;; whether we're interested in something this peer has
+   (interestedp :initform nil :accessor interestedp)
+   ;; whether this peer is interested in something we have
+   ;; if true then requests will be incoming when we unchoke
+   (interested-in-us-p :initform nil :accessor interested-in-us-p)))
 
 (defvar *peer-list* (make-hash-table :test #'equalp)
   "A hash table containing info_hashes as keys and hash tables mapping IP
