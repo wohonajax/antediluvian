e0fe0d9e9c32f66c288373d37934963df5f2d8b1
Hunter Chandler <wohonajax@gmail.com>
2025-10-13 16:09:27 -0500

    *PEER-LIST* now stores peer objects rather than socket futures

--- dht/peers.lisp
+++ dht/peers.lisp
@@ -11,8 +11,8 @@
   "Returns a list of address/port byte vectors for peers under INFO-HASH in
 compact peer format."
   (when-let (peers (gethash info-hash *peer-list*))
-    (loop for socket-future being the hash-values of peers
+    (loop for peer being the hash-values of peers
             using (hash-key ip)
-          for socket = (force socket-future)
+          for socket = (force (peer-socket peer))
           when socket ; if the connection failed, don't include that peer
             collect (compact-peer-info ip (get-peer-port socket)))))
