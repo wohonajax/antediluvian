3c3705334de96ed568ff3654cb57463fb324a159
Hunter Chandler <wohonajax@gmail.com>
2025-10-17 17:27:19 -0500

    Move peer connection code to its own file

---
+++ connect.lisp
@@ -0,0 +1,67 @@
+;;;; Code related to peer connections over TCP
+
+(in-package #:antediluvian)
+
+(defvar *listening-peer-socket* nil
+  "A TCP socket listening for connections from peers.")
+
+(defvar *peer-connection-threads* (list)
+  "A list of threads communicating with peer sockets.")
+
+(defun start-listener-thread ()
+  "Starts a thread that will listen for incoming connections on the listening
+peer socket."
+  (make-thread
+   (lambda ()
+     (loop for socket in (wait-for-input *listening-peer-socket* :ready-only t)
+           do (accept-peer-connection socket)))))
+
+(defvar *peer-listener-thread* nil
+  "A thread listening for incoming TCP connections.")
+
+(defun accept-peer-connection (socket)
+  "Accepts a peer connection from a SOCKET and listens in a new thread."
+  (when-let* ((accepted-socket (socket-accept socket))
+              (peer (receive-handshake accepted-socket)))
+    (push (make-thread (lambda ()
+                         (loop with stream = (socket-stream accepted-socket)
+                               ;; FIXME: wait for input?
+                               do (read-peer-wire-message peer stream))))
+          *peer-connection-threads*)))
+
+(defun initiate-peer-connection (peer)
+  "Initiates a TCP socket connection with PEER."
+  (macrolet ((try-socket-connection ()
+               `(handler-case (socket-connect (peer-ip peer)
+                                              (peer-port peer)
+                                              :element-type '(unsigned-byte 8)
+                                              :timeout 10)
+                  ;; if the connection fails, abandon the peer
+                  (connection-refused-error () (return-from thread-block))
+                  (timeout-error () (return-from thread-block)))))
+    (push (make-thread (lambda ()
+                         (block thread-block
+                           (setf (peer-socket peer) (try-socket-connection))
+                           (loop with socket = (peer-socket peer)
+                                 with stream = (socket-stream socket)
+                                 with torrent = (peer-torrent peer)
+                                 initially (unless (perform-handshake peer)
+                                             ;; perform-handshake closes the
+                                             ;; socket and removes the peer
+                                             ;; from the peer list if the
+                                             ;; handshake fails, so in that
+                                             ;; case just exit the whole block
+                                             (return-from thread-block))
+                                 initially (send-bitfield-message torrent socket)
+                                 initially (when (supports-bittorrent-dht-p peer)
+                                             (send-port-message socket))
+                                 initially (progn (send-unchoke-message socket)
+                                                  (setf (am-choking-p peer) nil))
+                                 unless (am-choking-p peer)
+                                   do (read-peer-wire-message peer stream)
+                                 ;; abandon the peer
+                                 finally (with-lock-held (*peer-list-lock*)
+                                           (socket-close socket)
+                                           (setf *peer-list*
+                                                 (remove peer *peer-list* :count 1)))))))
+          *peer-connection-threads*)))
