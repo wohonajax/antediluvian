dc03e7b1b2a2c9167259556f4c6e385821c46658
Hunter Chandler <wohonajax@gmail.com>
2025-11-02 20:31:28 -0600

    Get the piece length from the info dictionary of the torrent's "info" slot rather than from the info slot directly

--- peer-wire.lisp
+++ peer-wire.lisp
@@ -315,7 +315,8 @@ this DHT node is listening on."
 (defun request-piece (torrent piece-index socket)
   "Sends request messages for the PIECE-INDEXth piece of TORRENT to the peer
 connected to SOCKET."
-  (loop with piece-length = (gethash "piece length" (torrent-info torrent))
+  (loop with piece-length = (gethash "piece length"
+                                     (gethash "info" (torrent-info torrent)))
         with bytes-requested-so-far = 0
         with current-request-length = +length-offset+
         until (= bytes-requested-so-far piece-length)
