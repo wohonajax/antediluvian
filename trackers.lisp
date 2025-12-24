7eefe1af6d4f9b0f8b3885eb2aad283881053d96
Hunter Chandler <wohonajax@gmail.com>
2025-12-24 15:01:46 -0600

    Parse responses to announce GET requests, adding peers to the peer list and initiating connections

--- trackers.lisp
+++ trackers.lisp
@@ -22,13 +22,32 @@
     ;; TODO: determine whether to send started, stopped, or completed
     (princ "&event=started" str)))
 
+(defun parse-announce-response (response-string info-hash)
+  "Parses RESPONSE-STRING, a Bencoded string result from an announce GET
+request. Returns a list of peers."
+  (let* ((bencode:*binary-key-p* (rcurry #'equal '("peers")))
+         (peers-vector (gethash "peers" (bencode:decode response-string)))
+         (total-length (length peers-vector))
+         (results-list))
+    (do ((i 0 (+ i 6)))
+        ((= i total-length) results-list)
+      (multiple-value-bind (ip port)
+          (parse-node-ip (subseq peers-vector i (+ i 6)))
+        (let ((peer (make-peer ip port info-hash)))
+          (with-lock-held (*peer-list-lock*)
+            (unless (member peer *peer-list* :key #'peer-ip :test #'equalp)
+              (push peer *peer-list*)
+              (initiate-peer-connection peer))))))))
+
 (defun announce-to-tracker (torrent announce-url)
   "Sends an announce GET request for TORRENT to ANNOUNCE-URL. Returns NIL if
 the request fails."
-  (handler-case (dex:get (build-tracker-announce-url announce-url
-                                                     (torrent-info-hash torrent))
-                         :keep-alive nil)
-    (error ())))
+  (let ((info-hash (torrent-info-hash torrent)))
+    (handler-case (parse-announce-response
+                   (dex:get (build-tracker-announce-url announce-url info-hash)
+                            :keep-alive nil)
+                   info-hash)
+      (error ()))))
 
 (defun torrent-announce (torrent)
   "Sends announce GET requests for TORRENT to its associated trackers."
