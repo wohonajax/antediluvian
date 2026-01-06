810563b621d2ae0f94a4e5fb9d885302364c5c02
Hunter Chandler <wohonajax@gmail.com>
2026-01-06 15:06:56 -0600

    Handle reaching the end of the list when inserting a new element

--- doubly-linked-lists.lisp
+++ doubly-linked-lists.lisp
@@ -5,29 +5,35 @@
 (defun insert (item doubly-linked-list predicate &key (key #'identity))
   "Inserts ITEM into DOUBLY-LINKED-LIST in the correct position such that it
 will be sorted according to PREDICATE."
-  (let ((head (doubly-linked-list:head doubly-linked-list)))
-    (unless head
-      (return-from insert (doubly-linked-list:make-list item)))
-    (unless (doubly-linked-list:next head)
-      (return-from insert (let ((head-value (doubly-linked-list:value head)))
-                            (if (funcall predicate
-                                         (funcall key item)
-                                         (funcall key head-value))
-                                (doubly-linked-list:make-list item head-value)
-                                (doubly-linked-list:make-list head-value item))))))
-  (loop for current-node = (doubly-linked-list:head doubly-linked-list)
-          then (doubly-linked-list:next current-node)
-        for previous = nil then (doubly-linked-list:previous current-node)
-        when (funcall predicate
-                      (funcall key item)
-                      (funcall key (doubly-linked-list:value current-node)))
-          do (let ((new-node (make-instance 'doubly-linked-list:node
-                                            :value item
-                                            :previous previous
-                                            :next current-node)))
-               (setf (doubly-linked-list:next previous) new-node
-                     (doubly-linked-list:previous current-node) new-node)
-               (return doubly-linked-list))))
+  (flet ((compare (item1 item2)
+           (funcall predicate (funcall key item1) (funcall key item2))))
+    (let ((head (doubly-linked-list:head doubly-linked-list)))
+      (unless head
+        (return-from insert (doubly-linked-list:make-list item)))
+      (unless (doubly-linked-list:next head)
+        (return-from insert (let ((head-value (doubly-linked-list:value head)))
+                              (if (compare item head-value)
+                                  (doubly-linked-list:make-list item head-value)
+                                  (doubly-linked-list:make-list head-value item))))))
+    (loop for current-node = (doubly-linked-list:head doubly-linked-list)
+            then (doubly-linked-list:next current-node)
+          for previous = (doubly-linked-list:previous current-node)
+          while current-node
+          when (compare item (doubly-linked-list:value current-node))
+            do (let ((new-node (make-instance 'doubly-linked-list:node
+                                              :value item
+                                              :previous previous
+                                              :next current-node)))
+                 (setf (doubly-linked-list:next previous) new-node
+                       (doubly-linked-list:previous current-node) new-node)
+                 (return doubly-linked-list))
+          ;; we've reached the end of the list
+          finally (let ((new-node (make-instance 'doubly-linked-list:node
+                                                 :value item
+                                                 :previous current-node)))
+                    (setf (doubly-linked-list:next current-node) new-node
+                          (doubly-linked-list:tail doubly-linked-list) new-node)
+                    (return doubly-linked-list)))))
 
 (defun pop-from-end (doubly-linked-list)
   "Removes the last element from DOUBLY-LINKED-LIST."
