7cc9c5d34948399e6fd57868b0eab041afb1e099
Hunter Chandler <wohonajax@gmail.com>
2026-01-17 12:48:21 -0600

    Move +ALPHA+ to global.lisp

--- dht/global.lisp
+++ dht/global.lisp
@@ -3,3 +3,6 @@
 (defconstant +k+ 8
   "Replication parameter. Used for bucket size limits, how many nodes to return
 or search for, and more.")
+
+(defconstant +alpha+ 3
+  "The number of simultaneous lookups to perform.")
