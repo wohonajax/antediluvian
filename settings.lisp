827b0034d5cfbb708dbaac9fb576fcbd907eb2de
Hunter Chandler <wohonajax@gmail.com>
2026-01-18 19:25:01 -0600

    Rename the settings file to "config", like StumpWM, to avoid the highly-unlikely scenario where someone installs antediluvian to ~/.config/antediluvian

--- settings.lisp
+++ settings.lisp
@@ -17,7 +17,7 @@ configuration directory."
   #-mezzano (xdg-config-home relative-path)
   #+mezzano (merge-pathnames relative-path (user-homedir-pathname)))
 
-(defvar *settings-location* (settings-pathspec (filepaths:join "antediluvian" "settings.lisp"))
+(defvar *settings-location* (settings-pathspec (filepaths:join "antediluvian" "config"))
   "Where to store and load settings.")
 
 (defvar *download-directory*
