2cac5bc9fc62e672b4718c9340ab12972d65d61d
Hunter Chandler <wohonajax@gmail.com>
2026-01-19 20:13:42 -0600

    We don't use BYTE-ARRAY-TO-ASCII-STRING

--- util.lisp
+++ util.lisp
@@ -31,10 +31,6 @@
 (defun make-octets (length &rest args)
   "Makes a vector of length LENGTH with an element type of (UNSIGNED-BYTE 8)."
   (apply #'make-array length :element-type '(unsigned-byte 8) args))
-
-(defun byte-array-to-ascii-string (byte-array)
-  "Converts BYTE-ARRAY to an ASCII string."
-  (map 'string #'code-char byte-array))
 #+mezzano
 (defun random-data (num-bytes)
   "Generates an (unsigned-byte 8) vector of length NUM-BYTES filled with
