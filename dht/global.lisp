017a562a7f168414d427a649ff9f57271ead3cf8
Hunter Chandler <wohonajax@gmail.com>
2026-01-20 00:23:25 -0600

    Use DEFVAR instead of DEFCONSTANT

--- dht/global.lisp
+++ dht/global.lisp
@@ -1,8 +1,8 @@
 (in-package #:antediluvian)
 
-(defconstant +k+ 8
+(defvar +k+ 8
   "Replication parameter. Used for bucket size limits, how many nodes to return
 or search for, and more.")
 
-(defconstant +alpha+ 3
+(defvar +alpha+ 3
   "The number of simultaneous lookups to perform.")
